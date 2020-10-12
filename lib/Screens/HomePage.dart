3ce5ed6ce63c40584e9259313c19fd719994f1ca
Hathainuch626 <Hathainuch.NH@gmail.com>
2020-10-12 17:16:43 +0700

    up date

---
+++ lib/Screens/HomePage.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:myown/Screens/SignIn.dart';
+ 
+class HomePage extends StatefulWidget {
+  final FirebaseUser user;
+
+ 
+  HomePage(this.user, {Key key}) : super(key: key);
+ 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+ 
+class _HomePageState extends State<HomePage> {
+   final FirebaseAuth _auth = FirebaseAuth.instance;
+  @override
+  initState() {
+    super.initState();
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xff00796b) ,
+          title: Text("My Firebase App", style: TextStyle(color: Colors.white)),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.exit_to_app),
+                color: Colors.white,
+                onPressed: () {
+                   signOut(context);
+                })
+          ],
+        ),
+        backgroundColor: Colors.green[200],
+        body: Container(
+            child: Center(
+                child:
+                    Column(mainAxisSize: MainAxisSize.min,children: <Widget>[
+          Text("Hello Hathainuch", style: TextStyle(fontSize: 30, color: Colors.white)),
+          Text(widget.user.email, style: TextStyle(fontSize: 20,color: Colors.white)),
+        ]))));
+  }
+  
+  void signOut(BuildContext context) {
+    _auth.signOut();
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+        ModalRoute.withName('/'));
+  }
+}
+ 
---
+++ lib/Screens/SignUp.dart
@@ -0,0 +1,178 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+
+
+class SignUp extends StatefulWidget {
+  SignUp({Key key}) : super(key: key);
+ 
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp>{
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          
+          padding: EdgeInsets.only(left: 16,right: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 50,),
+                  Text("สร้างบัญชีผู้ใช้ใหม่",style: TextStyle(fontSize: 26,fontWeight: FontWeight.bold, color: Colors.white),),
+                  SizedBox(height: 6,),
+                  Text("โดยการใช้อีเมลล์ของคุณ",style: TextStyle(fontSize: 20,color: Colors.white),),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    color:  Color(0xFFFFFFFf),
+                    child: TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: "อีเมลล์",
+                        labelStyle: TextStyle(fontSize: 20,color: Colors.grey.shade400,fontWeight: FontWeight.w600),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.grey.shade300),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  Container(
+              color:  Color(0xFFFFFFFf),
+
+                    child: TextField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        labelText: "รหัสผ่าน",
+                        labelStyle: TextStyle(fontSize: 20,color: Colors.grey.shade400,fontWeight: FontWeight.w600),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.grey.shade300),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                  Container(
+                  color:  Color(0xFFFFFFFf),
+
+                    child: TextField(
+                      controller: confirmController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "กรอกรหัสผ่านอีกครั้ง",
+                        labelStyle: TextStyle(fontSize: 20,color: Colors.grey.shade400,fontWeight: FontWeight.w600),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.grey.shade300),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Container(
+                    height: 50,
+                    child: FlatButton(
+                      onPressed: (){
+                        signUp();
+                      },
+                      padding: EdgeInsets.all(0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Ink(
+                      
+                        child: Container(
+                          alignment: Alignment.center,
+                          constraints: BoxConstraints(minHeight: 50,maxWidth: double.infinity),
+                          child: Text("สมัครสมาชิก",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),textAlign: TextAlign.center,),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16,),
+                 
+                  SizedBox(height: 30,),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("เป็นสมาชิกอยู่แล้ว?",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.pop(context);
+                      },
+                      child: Text("เข้าสู่ระบบ",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+signUp() {
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+    String confirmPassword = confirmController.text.trim();
+    if (password == confirmPassword && password.length >= 6) {
+      _auth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((user) {
+        print("Sign up user successful.");
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage(user)),
+            ModalRoute.withName('/'));
+      }).catchError((error) {
+         print(error.message);
+      });
+    } else {
+      print("Password and Confirm-password is not match.");
+    }
+  }
+
+
+}
